990f16014fd73ea363a8bbd2909ba9cb1f954aff
Shayankhan <shayankhan9148@gmail.com>
2024-03-03 17:08:48 +0500

    its difficult

---
+++ app.js
@@ -0,0 +1,241 @@
+// // 
+// var num = 1
+// alert(num)
+// // num =num+1;
+// // num = num+ num;
+// num=2
+// alert(num )
+
+// num=num+1;
+
+// alert(num)
+// num=num+1;
+
+// alert(num)
+// num=num+1;
+
+// alert(num)
+// num=num+1;
+
+// alert(num)
+// CHAPTER 4
+// question1
+// var a = "shayan"
+// var b =" khan"
+// var c = "muhammad"
+// document.write( "<h1>" + a+b+c+"</h1>")
+//  question2 
+// var myName=("")
+// var myFatherName=("")
+// var myAge=("")
+// var myProfession=("")
+// var myHobbies=("")
+
+// var console=("")
+// var name=("")
+// var document=("")
+// var source=("")
+// var let=("")
+//   question 3
+// var myHading =("<h1> Rules for naming JS variables </h1>")
+
+// document.write(myHading)
+
+
+
+// var myName=("shayan")
+// var age=(22)
+// var myProfession=("learn in smit")
+// var others=("studying in KU")
+
+
+// document.write(myName+age+myProfession+others)
+
+
+// var $myName=("shayan")
+// var _name =("khan")
+// var age=(22)
+// document.write($myName+_name+age)
+
+
+
+// var shayan=("khan")
+// var SHAYAN=("KHAN")
+// document.write(shayan+SHAYAN)
+//    chapter5
+// question 1
+// var a=(3)
+// var b=(5)
+// document.write(a+b)
+//quesytion 2 
+// var a=(3)
+//  var b=(5)
+//  document.write(a-b)
+
+// var a=(2)
+//  var b=(4)
+//  document.write(a*b)
+
+// var a=(32)
+//  var b=(4)
+//  document.write(a/b)
+
+
+
+// var a=(80)
+// var b=(24)                      
+// document.write(a%b)
+//question 3
+
+
+// var myVariable;
+
+// document.write("Value after variable declaration is: " + myVariable+ "</br>" );
+
+// myVariable = 5;
+
+// document.write("Initial value: " + myVariable + "<br>");
+
+// myVariable++;
+
+// document.write("value after increment is:" + myVariable+ "</br>")
+
+// myVariable= myVariable+7;
+// document.write("value after adding is:" + myVariable+ "</br>")
+
+// myVariable--;
+
+// document.write("value after decrement is:" + myVariable+ "</br>")
+
+// var reminder=myVariable % 3;
+
+// document.write("the reminder is:"+ reminder+ "</br>")
+
+// question 4
+
+// var ticket=600
+// var totalTicket=5
+
+// document.write("total coast to buy 5 ticket is" + ticket*totalTicket+"PKR")
+
+//QUESTION 5
+
+//dont understand
+
+//question 6
+
+// var celsiusTemperature=25;
+// var fahrenheitTemperature = (celsiusTemperature * 9/5) + 32;
+// document.write(celsiusTemperature + "°C is " + fahrenheitTemperature + "°F" + "<br>");
+
+// var fahrenheitTemp = 77;
+// var celsiusTemp = (fahrenheitTemp - 32) * 5/9;
+// document.write(fahrenheitTemp + "°F is " + celsiusTemp + "°C");
+
+// QUESTION 7
+
+
+// var priceItem1 = 650;
+
+// var priceItem2 = 200;
+
+// var quantityItem1 = 3;
+
+// var quantityItem2 = 1;
+
+// var shippingCharges = 100;
+
+// var totalCost = (priceItem1 * quantityItem1) + (priceItem2 * quantityItem2) + shippingCharges;
+// document.write("<h2>Receipt</h2>");
+// document.write("<p>Price of Item 1: " + priceItem1 + "</p>");
+// document.write("<p>Quantity of Item 1: " + quantityItem1 + "</p>");
+// document.write("<p>Price of Item 2: " + priceItem2 + "</p>");
+// document.write("<p>Quantity of Item 2: " + quantityItem2 + "</p>");
+// document.write("<p>Shipping Charges: " + shippingCharges + "</p>");
+// document.write("<h3>Total Cost: " + totalCost + "</h3>");
+
+// question 8
+
+
+// var totalMarks = 980;
+// var marksObtained = 804;
+// var percentage = (marksObtained / totalMarks) * 100;
+
+// document.write("<h2>Percentage Calculation</h2>");
+// document.write("<p>Total Marks: " + totalMarks + "</p>");
+// document.write("<p>Marks Obtained: " + marksObtained + "</p>");
+// document.write("<h3>Percentage Obtained: " + percentage.toFixed(2) + "%</h3>");
+
+
+// question 9
+
+
+// var dollar=10;
+// var riyal=25;
+// var oneDollar=104;
+// var oneRiyal=28;
+// var total=(dollar*oneDollar)+(riyal*oneRiyal)
+// document.write("total currency in PKR:"+total+"</br>" )
+
+
+//question 10
+// var x=0
+// var a= 5;
+// var b=10;
+// var c=2;
+// var total=((x+a)*b)/c
+// document.write("total number"+total+"<br>")
+
+// question 11
+
+// var current=2016;
+// var born=1992;
+// var age=current-born;
+
+// document.write("current year:"+current+"<br>")
+// document.write("birth year:"+born+"<br>")
+// document.write("your age is:"+age+"<br>")
+
+//question 12
+
+
+// var radius=20;
+// var pi= 3.142;
+// var cf=2*radius*pi
+
+// var ar=pi*(radius*radius)
+// document.write("radius of circle"+radius+"</br>")
+// document.write("circumference of circle"+cf+"</br>")
+// document.write("area of circle"+ar+"</br>")
+
+// question 13
+
+
+// var snack="choclate chips";
+// var currentAge=15;
+// var bee=65;
+// var amount=3;
+
+// var yearsRemaining = bee - currentAge;
+// var daysRemaining = yearsRemaining * 365;
+// var totalSnacks = daysRemaining * amount;
+
+// document.write("favourite snake :"+snack+"<br>")
+// document.write(" current age: "+currentAge+"</br")
+// document.write(" estimated maximum age: "+bee+"<br>")
+// document.write(" amount of anack per days: "+amount+"<br>")
+// document.write("you will need"+totalSnacks+"chocalte chips to lat youuntil the ripe old age of"+bee+"<br>")
+
+
+                          //chapter6
+                
+// var a=10;
+
+// document.write("result:"+"</br>"+"the value of a is"+ a +"</br>")
+
+// var a=10;
+// ++a
+// document.write("result:"+"</br>"+"the value of a is"+ a +"</br>")
+
+
+
